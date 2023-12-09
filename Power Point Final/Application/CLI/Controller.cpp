5e02bd068aa50fc35b9f07b04faebaf243e07b8e
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-09 13:39:03 +0400

    Update Controller.cpp

--- Power Point Final/Application/CLI/Controller.cpp
+++ Power Point Final/Application/CLI/Controller.cpp
@@ -2,7 +2,7 @@
 
 void Controller::run()
 {
-    bool isExit = true; // TK: May be wise versa? isExit = false, otherwise your loop never will be executed 
+    bool isExit = true; // TK: May be wice versa? isExit = false, otherwise your loop never will be executed 
     Command cmd;
     while (!isExit)
     {
