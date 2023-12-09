9dafaa185c8dae950ec656a56d091f39cf0cf065
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-09 13:25:29 +0400

    Comments

--- Power Point Final/Application/Application.hpp
+++ Power Point Final/Application/Application.hpp
@@ -9,8 +9,11 @@ class Application
 private:
     Controller *controller;
 
+    // TK Application should have exec() method where it will call Controllers run method
 public:
     Controller* getController();
+
+    // Application also should keep the Document & Director
 };
 
 #endif // APPLICATION_HPP
